1556f2097439eb6439bac4cb8f3ecc0fc885f4fe
Kaique Lira <Kaiqueliira@gmail.com>
2022-09-08 16:38:28 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,28 @@
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+
+export default function Home() {
+  function handleParcitipantAdd() {
+    console.log("aqui");
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}> Nome do Evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de novembro de 2022.</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do Participante"
+          placeholderTextColor="#6b6b6b"
+          keyboardType="default"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParcitipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
